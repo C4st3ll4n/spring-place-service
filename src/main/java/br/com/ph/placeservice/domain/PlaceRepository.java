643a7916fb0ee37521eb90ccaa5f84f39a30a444
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 19:38:50 -0300

    refactor: adjust place repository

--- src/main/java/br/com/ph/placeservice/domain/PlaceRepository.java
+++ src/main/java/br/com/ph/placeservice/domain/PlaceRepository.java
@@ -1,8 +1,12 @@
 package br.com.ph.placeservice.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface PlaceRepository extends ReactiveCrudRepository<Place, UUID> {
+public interface PlaceRepository extends ReactiveCrudRepository<Place, UUID>, ReactiveQueryByExampleExecutor<Place>, R2dbcRepository<Place, UUID> {
 }
