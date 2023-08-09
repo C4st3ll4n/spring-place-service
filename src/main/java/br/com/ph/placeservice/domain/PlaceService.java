07c0b584eb104a327e60096a40e88866a5d0d72a
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 19:39:08 -0300

    feat: add place service

---
+++ src/main/java/br/com/ph/placeservice/domain/PlaceService.java
@@ -0,0 +1,24 @@
+package br.com.ph.placeservice.domain;
+
+import br.com.ph.placeservice.api.PlaceRequest;
+import com.github.slugify.Slugify;
+import reactor.core.publisher.Mono;
+
+public class PlaceService {
+    private PlaceRepository repository;
+    private Slugify slugify;
+
+    public PlaceService(PlaceRepository repository) {
+        this.repository = repository;
+        this.slugify = Slugify.builder().build();
+    }
+
+    public Mono<Place> create(PlaceRequest placeRequest) {
+        var place = new Place(
+                null, placeRequest.name(),
+                slugify.slugify(placeRequest.name()), placeRequest.state(),
+                placeRequest.createdAt(), placeRequest.updatedAt()
+        );
+        return repository.save(place);
+    }
+}
