28c90de81163ea266e6c2277650aa14e4ad7beee
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 19:37:56 -0300

    feat: add automatic date creation

--- src/main/java/br/com/ph/placeservice/domain/Place.java
+++ src/main/java/br/com/ph/placeservice/domain/Place.java
@@ -1,9 +1,13 @@
 package br.com.ph.placeservice.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record Place(
-        UUID id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt
+        UUID id, String name, String slug, String state,
+        @CreatedDate LocalDateTime createdAt, @LastModifiedDate LocalDateTime updatedAt
 ) {
 }
