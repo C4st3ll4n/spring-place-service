fb40fef7b43411b21c79bfa8e2841bc45886c789
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 23:46:30 -0300

    test: add success and fail cenarios

--- src/test/java/br/com/ph/placeservice/PlaceServiceApplicationTests.java
+++ src/test/java/br/com/ph/placeservice/PlaceServiceApplicationTests.java
@@ -1,13 +1,44 @@
 package br.com.ph.placeservice;
 
+import br.com.ph.placeservice.api.PlaceRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void testCreatePlaceSuccess() {
+        var name = "Valid name";
+        var state = "Valid state";
+
+        webTestClient.post().uri("/places")
+                .bodyValue(new PlaceRequest(
+                        name, state
+                )).exchange().expectBody()
+                .jsonPath("name").isEqualTo(name)
+                .jsonPath("state").isEqualTo(state)
+                .jsonPath("slug").isNotEmpty()
+                .jsonPath("createdAt").isNotEmpty()
+                .jsonPath("updatedAt").isNotEmpty();
+    }
+
+    @Test
+    public void testCreatePlaceFails(){
+        var state = "";
+        var name = "";
+
+        webTestClient.post().uri("/places")
+                .bodyValue(new PlaceRequest(
+                        name, state
+                )).exchange()
+                .expectStatus().isBadRequest();
+    }
+
 
 }
